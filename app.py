f593fb67ed80ffd853ff64ee479a861004d70e05
deciever5 <marcinkunikowski@gmail.com>
2022-11-02 14:37:50 +0100

    add message page

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask import request, redirect
 
 app = Flask(__name__)
 
@@ -7,3 +9,19 @@ app = Flask(__name__)
 def hello():
     my_name = "John"
     return f'Hello, {my_name}!'
+
+
+@app.route('/blog/<id>')
+def blog_main(id):
+    return f"This is a  blog entry #{id}"
+
+
+@app.route('/message', methods=['GET', 'POST'])
+def message():
+    if request.method == 'GET':
+        print("We received GET")
+        return render_template("form.html")
+    elif request.method == 'POST':
+        print("We received POST")
+        print(request.form)
+        return redirect("/")
