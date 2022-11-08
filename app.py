38699441ceca95fc4b8744c32b3b7ff046804966
Marcin Kunikowski <marcinkunikowski@gmail.com>
2022-11-09 00:32:30 +0100

    added form functionality

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -12,8 +11,14 @@ def about_me():
 
 @app.route("/", methods=['GET', 'POST'])
 def contact():
+    if request.method == "POST":
+        username = request.form["username"]
+        comment = request.form["comment"]
+        print(f"User {username} wrote {comment}")
     return render_template("/mypage/contact.html")
 
+
+# ,say=request.form['comment'], to=request.form['username']
 
 if __name__ == "__main__":
     app.run()
